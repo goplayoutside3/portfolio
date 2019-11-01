146bf1e9f4212263163043f790efa9175aa39db1
Delilah Clement <delilah@reachcreative.com>
2019-11-01 14:26:14 -0500

    add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import App from 'next/app'
 import Layout from '../components/Layout'
+import Navbar from '../components/Layout'
+import { PageTransition } from 'next-page-transitions'
+
 import '../styles/styles.scss'
 
 export default class MyApp extends App {
-  render () {
+  render() {
     const { Component, pageProps, router } = this.props
     return (
       <Layout>
-        <Component {...pageProps} />
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component {...pageProps} key={router.route} />
+        </PageTransition>
       </Layout>
     )
   }
