99bc78527828c99d435db3f9f5cfbe5fdd4a4b3f
Delilah Clement <delilah@reachcreative.com>
2019-11-01 11:12:24 -0500

    favicon href

--- components/Layout.js
+++ components/Layout.js
@@ -9,8 +9,8 @@ const Layout = ({ children, title = 'Delilah Clement', description }) => (
   <div>
     <Head>
       <title>{title}</title>
-      <link rel="shortcut icon" href={`/images/longboard.svg`} />
-      <link rel="icon" href={`/images/longboard.svg`} />
+      <link rel="shortcut icon" href={`${siteUrl}/images/longboard.svg`} />
+      <link rel="icon" href={`${siteUrl}/images/longboard.svg`} />
 
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="description" content={`${description} | ${title}`} />
