28f02ce25c43efa307a11ada114c5bec7a4fd0c0
Delilah Clement <delilah@reachcreative.com>
2019-12-17 12:40:59 -0600

    style home hero w/ embedded svg

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Homepage = () => (
-    <main className="home">
-      <section className="container hero">
-        <img className="hero-bg" src="/images/home-hero-desktop.svg" />
-        <h1>
-          <span className="name">I'm Delilah</span>
-          <span>a front-end web developer</span>
-        </h1>
-      </section>
-    </main>
+  <main className="home">
+    <section className="hero">
+      <img className="hero-bg" src="/images/home-hero-desktop2.svg" />
+      <div className="hero-copy">
+        <h1>I'm Delilah</h1>
+        <p>a front-end web developer</p>
+      </div>
+    </section>
+  </main>
 )
 
 export default Homepage
