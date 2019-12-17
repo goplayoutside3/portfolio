072b7286d56a19e954817b985ba4a15c957e5922
Delilah Clement <delilah@reachcreative.com>
2019-12-17 13:39:56 -0600

    test deploy prod

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Homepage = () => (
       <img className="hero-bg mobile" src="/images/home-hero-mobile.svg" />
       <div className="hero-copy">
         <h1>I'm Delilah</h1>
-        <p>a front-end web developer</p>
+        <p>a front-end web developer </p>
       </div>
     </section>
   </main>
