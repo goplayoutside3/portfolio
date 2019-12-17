f1b9c86681d1f3ce2c8e5c0bc1dafd836a879e3d
Delilah Clement <delilah@reachcreative.com>
2019-12-17 13:07:09 -0600

    add mobile home hero styling

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Homepage = () => (
   <main className="home">
     <section className="hero">
-      <img className="hero-bg" src="/images/home-hero-desktop2.svg" />
+      <img className="hero-bg desktop" src="/images/home-hero-desktop.svg" />
+      <img className="hero-bg mobile" src="/images/home-hero-mobile.svg" />
       <div className="hero-copy">
         <h1>I'm Delilah</h1>
         <p>a front-end web developer</p>
