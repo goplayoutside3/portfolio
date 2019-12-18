821575882aa479b2aeca99feb69143be39074d8f
Delilah Clement <delilah@reachcreative.com>
2019-12-18 11:55:54 -0600

    style toolbox base to red

--- components/Toolbox.js
+++ components/Toolbox.js
@@ -2,9 +2,9 @@ const Toolbox = () => {
   return (
     <section className="toolbox">
       <div className="box-cont">
-        <img className="box back" src="/images/box-back.svg" alt="Toolbox" />
+        <img className="box back" src="/images/box-back.svg" alt="" />
         <img className="box front" src="/images/box-front.svg" alt="Toolbox" />
-        <p className="technologies">Technologies</p>
+        <h3 className="technologies">Technologies</h3>
       </div>
     </section>
   )
