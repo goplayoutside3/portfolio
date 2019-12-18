59643af3086bfd4f60573ecff3d8a4ca617324c0
Delilah Clement <delilah@reachcreative.com>
2019-12-18 12:28:28 -0600

    build hammer svg

--- components/Toolbox.js
+++ components/Toolbox.js
@@ -3,6 +3,14 @@ const Toolbox = () => {
     <section className="toolbox">
       <div className="box-cont">
         <img className="box back" src="/images/box-back.svg" alt="" />
+        <div className="tool-cont hammer">
+          <img
+            className="tool"
+            src="/images/hammer.svg"
+            alt="Hammer"
+          />
+          <p className="label javascript">Javascript</p>
+        </div>
         <img className="box front" src="/images/box-front.svg" alt="Toolbox" />
         <h3 className="technologies">Technologies</h3>
       </div>
