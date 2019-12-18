e63d04bb107d91c6a7a753b64fcb5ca0c4b63c16
Delilah Clement <delilah@reachcreative.com>
2019-12-18 14:01:21 -0600

    build palette svg

--- components/Toolbox.js
+++ components/Toolbox.js
@@ -4,15 +4,22 @@ const Toolbox = () => {
       <div className="box-cont">
         <img className="box back" src="/images/box-back.svg" alt="" />
         <div className="tool-cont hammer">
-          <img
-            className="tool"
-            src="/images/hammer.svg"
-            alt="Hammer"
-          />
+          <img className="tool" src="/images/hammer.svg" alt="Hammer" />
           <p className="label javascript">Javascript</p>
         </div>
         <img className="box front" src="/images/box-front.svg" alt="Toolbox" />
         <h3 className="technologies">Technologies</h3>
+      </div>
+      <div className="box-cont">
+        <div className="tool-cont palette">
+          <img
+            className="tool"
+            src="/images/palette.svg"
+            alt="Art Palette"
+          />
+          <p className="label css">CSS</p>
+        </div>
+        <img className="" src="" alt="" />
       </div>
     </section>
   )
