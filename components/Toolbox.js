4a89e4707e582644fab6376f71ae550f076f3452
Delilah Clement <delilah@reachcreative.com>
2019-12-18 11:27:15 -0600

    build toolbox base

--- components/Toolbox.js
+++ components/Toolbox.js
@@ -1,7 +1,11 @@
 const Toolbox = () => {
   return (
     <section className="toolbox">
-
+      <div className="box-cont">
+        <img className="box back" src="/images/box-back.svg" alt="Toolbox" />
+        <img className="box front" src="/images/box-front.svg" alt="Toolbox" />
+        <p className="technologies">Technologies</p>
+      </div>
     </section>
   )
 }
