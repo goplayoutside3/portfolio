99f0bc1a8a06af3c75c79ddee7eb68e3713ddcfa
Delilah Clement <delilah@reachcreative.com>
2019-12-19 13:21:10 -0600

    add separate page curl layers

--- components/PageCurl.js
+++ components/PageCurl.js
@@ -2,7 +2,11 @@ const PageCurl = ({ image }) => {
   return (
     <div className="col">
       <div className="thumbnail" style={{ backgroundImage: `url(${image})` }} />
-      <img className="page-closed" src="/images/page-closed.svg" alt="" />
+      {/* <img className="page-closed" src="/images/page-curl-closed-bottom.svg" alt="" />
+      <img className="page-closed" src="/images/page-curl-closed-top.svg" alt="" /> */}
+      <img className="page-open" src="/images/page-curl-open-bottom.svg" alt="" />
+      <p className="title">Title Lorem Ipsem</p>
+      <img className="page-open" src="/images/page-curl-open-top.svg" alt="" />
     </div>
   )
 }
