2ee5c0f02484e95116bf064b0d5779cef134bf68
Delilah Clement <delilah@reachcreative.com>
2019-12-21 11:24:16 -0600

    convert PageCurl svg to all paths

--- components/PageCurl.js
+++ components/PageCurl.js
@@ -5,35 +5,33 @@ const pageColor = '#fdf6e4'
 const duration = 0.4
 
 const handleHover = z => {
-  // const openPageBottom = Tween.to(`#page-closed${z}`, duration, {
-  //   morphSVG: {shape: `#page-open${z}`, shapeIndex: 'auto'},
-  // })
-
-  // const openPageShadow = Tween.to(`#page-closed-shadow${z}`, duration, {
-  //   morphSVG: `#page-open-shadow${z}`,
-  // })
-
+  const openPageBottom = Tween.to(`#page-closed${z}`, duration, {
+    morphSVG: { shape: `#page-open${z}`, shapeIndex: 'auto' },
+  })
+  const openPageShadow = Tween.to(`#page-closed-shadow${z}`, duration, {
+    morphSVG: `#page-open-shadow${z}`,
+  })
   const openCurl = Tween.to(`#curl-closed${z}`, duration, {
     morphSVG: `#curl-open${z}`,
   })
-  // const openCurlShadow = Tween.to(`#curl-closed-shadow${z}`, duration, {
-  //   morphSVG: `#curl-open-shadow${z}`,
-  // })
+  const openCurlShadow = Tween.to(`#curl-closed-shadow${z}`, duration, {
+    morphSVG: `#curl-open-shadow${z}`,
+  })
 }
 
 const handleLeave = z => {
-  // const openPageBottom = Tween.to(`#page-closed${z}`, duration, {
-  //   morphSVG: {shape: `#page-closed${z}`, shapeIndex: '12'},
-  // })
-  // const openPageShadow = Tween.to(`#page-closed-shadow${z}`, duration, {
-  //   morphSVG: `#page-closed-shadow${z}`,
-  // })
+  const openPageBottom = Tween.to(`#page-closed${z}`, duration, {
+    morphSVG: { shape: `#page-closed${z}`, shapeIndex: '12' },
+  })
+  const openPageShadow = Tween.to(`#page-closed-shadow${z}`, duration, {
+    morphSVG: `#page-closed-shadow${z}`,
+  })
   const openCurl = Tween.to(`#curl-closed${z}`, duration, {
     morphSVG: `#curl-closed${z}`,
   })
-  // const openCurlShadow = Tween.to(`#curl-closed-shadow${z}`, duration, {
-  //   morphSVG: `#curl-closed-shadow${z}`,
-  // })
+  const openCurlShadow = Tween.to(`#curl-closed-shadow${z}`, duration, {
+    morphSVG: `#curl-closed-shadow${z}`,
+  })
 }
 
 const PageCurl = ({ image = '', z = '1' }) => {
@@ -50,29 +48,29 @@ const PageCurl = ({ image = '', z = '1' }) => {
         viewBox="0 0 517 605.01"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <polygon
+        <path
           id={`page-closed-shadow${z}`}
           className="page-closed-shadow"
-          points="377.27 604.51 172.77 605.01 41 5 377.27 4.51 377.27 604.51"
+          d="M377.3,604.5L172.8,605L41,5l336.3-0.5V604.5z"
           style={{ fill: 'gray', opacity: 0.7 }}
         />
-        <polygon
+        <path
           id={`page-open-shadow${z}`}
           className="page-open-shadow"
-          points="378.06 602.14 273 603 130 3 378.06 2.14 378.06 602.14"
+          d="M378.1,602.1L273,603L130,3l248.1-0.9V602.1z"
           style={{ fill: 'gray', opacity: 0.7 }}
         />
-        <polygon
+        <path
           id={`page-closed${z}`}
           className="page-closed"
-          points="44.23 3 399 2.5 399 602 198.94 602.5 44.23 3"
-          style={{ fill: pageColor, stroke: "black", strokeWidth: "5px" }}
+          d="M401.5,604.5L197,605L41,0.5L401.5,0V604.5z"
+          style={{ fill: pageColor }}
         />
-        <polygon
+        <path
           id={`page-open${z}`}
           className="page-open"
-          points="300.9 602.5 133.29 3.37 402.5 2.51 402.5 602.5 300.9 602.5"
-          style={{ fill: pageColor, stroke: "black", strokeWidth: "5px" }}
+          d="M405,605H299L130,0.9L405,0V605z"
+          style={{ fill: pageColor }}
         />
 
         <text className="title" x="180" y="80">
@@ -90,7 +88,7 @@ const PageCurl = ({ image = '', z = '1' }) => {
           height="600"
           style={{
             fill: 'none',
-            stroke: '#000',
+            stroke: 'black',
             strokeMiterlimit: 10,
             strokeWidth: '5px',
           }}
@@ -113,15 +111,14 @@ const PageCurl = ({ image = '', z = '1' }) => {
           id={`curl-closed${z}`}
           className="curl-closed"
           d="M44,2.51H55.67L362.29,457.28c-36.7.51-71.15,16.12-102.45,46.42-24.54,23.77-46.57,56.77-62.5,93.51Z"
-          style={{ fill: pageColor, stroke: "black", strokeWidth: "5px" }}
+          style={{ fill: pageColor, stroke: 'black', strokeWidth: '5px' }}
         />
         <path
           id={`curl-open${z}`}
           className="curl-open"
           d="M133.29,2.5h9.46L512,497.94h-1.06c-82.25,0-179.48,34.23-211.36,99.72Z"
-          style={{ fill: pageColor, stroke: "black", strokeWidth: "5px" }}
+          style={{ fill: pageColor, stroke: 'black', strokeWidth: '5px' }}
         />
-
       </svg>
     </div>
   )
