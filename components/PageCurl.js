9f18cfa822fbc68a691371bc9bea43d083a794f3
Delilah Clement <delilah@reachcreative.com>
2019-12-21 11:30:33 -0600

    separate title from PageCurl svg

--- components/PageCurl.js
+++ components/PageCurl.js
@@ -44,6 +44,7 @@ const PageCurl = ({ image = '', z = '1' }) => {
     >
       <div className="thumbnail" style={{ backgroundImage: `url(${image})` }} />
       <svg
+        id="closed-page-paths"
         className="page"
         viewBox="0 0 517 605.01"
         xmlns="http://www.w3.org/2000/svg"
@@ -55,31 +56,11 @@ const PageCurl = ({ image = '', z = '1' }) => {
           style={{ fill: 'gray', opacity: 0.7 }}
         />
         <path
-          id={`page-open-shadow${z}`}
-          className="page-open-shadow"
-          d="M378.1,602.1L273,603L130,3l248.1-0.9V602.1z"
-          style={{ fill: 'gray', opacity: 0.7 }}
-        />
-        <path
           id={`page-closed${z}`}
           className="page-closed"
           d="M401.5,604.5L197,605L41,0.5L401.5,0V604.5z"
           style={{ fill: pageColor }}
         />
-        <path
-          id={`page-open${z}`}
-          className="page-open"
-          d="M405,605H299L130,0.9L405,0V605z"
-          style={{ fill: pageColor }}
-        />
-
-        <text className="title" x="180" y="80">
-          Title Lorem
-        </text>
-        <text className="title" x="180" y="120">
-          Ipsum
-        </text>
-
         <rect
           id="guide"
           x="2.5"
@@ -93,7 +74,16 @@ const PageCurl = ({ image = '', z = '1' }) => {
             strokeWidth: '5px',
           }}
         />
+      </svg>
 
+      <p className="title">Title Lorem Ipsum</p>
+
+      <svg
+        id="closed-curl-paths"
+        className="page"
+        viewBox="0 0 517 605.01"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path
           id={`curl-closed-shadow${z}`}
           className="curl-closed-shadow"
@@ -101,18 +91,39 @@ const PageCurl = ({ image = '', z = '1' }) => {
           style={{ fill: 'gray', opacity: 0.7 }}
         />
         <path
+          id={`curl-closed${z}`}
+          className="curl-closed"
+          d="M44,2.51H55.67L362.29,457.28c-36.7.51-71.15,16.12-102.45,46.42-24.54,23.77-46.57,56.77-62.5,93.51Z"
+          style={{ fill: pageColor, stroke: 'black', strokeWidth: '5px' }}
+        />
+      </svg>
+
+      <svg
+        id="open-curl-paths"
+        className="page"
+        viewBox="0 0 517 605.01"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          id={`page-open-shadow${z}`}
+          className="page-open-shadow"
+          d="M378.1,602.1L273,603L130,3l248.1-0.9V602.1z"
+          style={{ fill: 'gray', opacity: 0.7 }}
+        />
+        <path
+          id={`page-open${z}`}
+          className="page-open"
+          d="M405,605H299L130,0.9L405,0V605z"
+          style={{ fill: pageColor }}
+        />
+
+        <path
           id={`curl-open-shadow${z}`}
           className="curl-open-shadow"
           d="M517,525c-79,7-189.41,2-219,74L130,3h14Z"
           style={{ fill: 'gray', opacity: 0.7 }}
         />
 
-        <path
-          id={`curl-closed${z}`}
-          className="curl-closed"
-          d="M44,2.51H55.67L362.29,457.28c-36.7.51-71.15,16.12-102.45,46.42-24.54,23.77-46.57,56.77-62.5,93.51Z"
-          style={{ fill: pageColor, stroke: 'black', strokeWidth: '5px' }}
-        />
         <path
           id={`curl-open${z}`}
           className="curl-open"
