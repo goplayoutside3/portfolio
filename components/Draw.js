d05e8a6a5ffc2cb293ea9d845c519b29e700dad4
Delilah Clement <delilah@reachcreative.com>
2020-01-21 16:29:01 -0600

    add skyline svg data

--- components/Draw.js
+++ components/Draw.js
@@ -4,7 +4,7 @@ import DrawSVGPlugin from '../gsap/DrawSVGPlugin.min.js'
 const Draw = () => {
   return (
       <div >
-
+        <img className="skyline" src="/images/chicago.svg" alt="Chicago skyline" />
       </div>
   )
 }
