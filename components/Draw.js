4b48df6b4a70d626c0a1dca69f472f3c7bbeb3e8
Delilah Clement <delilah@reachcreative.com>
2020-01-21 16:01:48 -0600

    add plugin and chicago.svg

---
+++ components/Draw.js
@@ -0,0 +1,12 @@
+import '../gsap/gsap.min.js'
+import DrawSVGPlugin from '../gsap/DrawSVGPlugin.min.js'
+
+const Draw = () => {
+  return (
+      <div >
+
+      </div>
+  )
+}
+
+export default Draw
--- pages/community.js
+++ pages/community.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Draw } from '../components'
 
 // add dance, safe streets & water advocacy
 
 const Community = () => (
     <main className="community">
       <section className="container hero">
-        <h1>Under Construction</h1>
+        {/* <h1>Under Construction</h1> */}
+        <Draw />
       </section>
     </main>
 )
