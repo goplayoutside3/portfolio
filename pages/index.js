414521ccac633e1935a00c74490bfa652bdc013c
Delilah Clement <delilah@reachcreative.com>
2020-07-16 13:17:58 -0500

    refactor homepage stylesheet

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import styles from '../styles/layouts/home.module.scss'
+import classes from 'classnames'
 
-const Homepage = () => {
+export default () => {
   return (
-    <main className="home">
-      <section className="hero">
-        <img className="hero-bg desktop" src="/images/home-hero-desktop.svg" />
-        <img className="hero-bg mobile" src="/images/home-hero-mobile.svg" />
-        <div className="hero-copy">
+    <main id={styles.home}>
+      <section className={styles.hero}>
+        <img className={styles["hero-bg-desktop"]} src="/images/home-hero-desktop.svg" />
+        <img className={styles["hero-bg-mobile"]} src="/images/home-hero-mobile.svg" />
+        <div className={styles["hero-copy"]}>
           <h1>I'm Delilah</h1>
           <p>a front-end web developer</p>
         </div>
       </section>
-      <section className="introduction">
+      <section className={styles.introduction}>
         <p>
           I work at <a href="https://www.reachcreative.com/" target="_blank">Reach Creative</a>, a Chicago-based design agency.
         </p>
@@ -19,5 +20,3 @@ const Homepage = () => {
     </main>
   )
 }
-
-export default Homepage
