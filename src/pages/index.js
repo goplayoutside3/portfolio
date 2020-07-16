f8c9297468e975935f0534a30bc3271f1f379ba3
Delilah Clement <delilah@reachcreative.com>
2020-07-16 14:11:01 -0500

    clean up homepage hero styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import classes from 'classnames'
 export default () => {
   return (
     <main id={styles.home}>
-      <section className={styles.hero}>
+      <section className={classes(styles.hero, 'hero')}>
         <img className={styles["hero-bg-desktop"]} src="/images/home-hero-desktop.svg" />
         <img className={styles["hero-bg-mobile"]} src="/images/home-hero-mobile.svg" />
         <div className={styles["hero-copy"]}>
           <h1>I'm Delilah</h1>
-          <p>a front-end web developer</p>
+          <h2 className="h2">a front-end web developer</h2>
         </div>
       </section>
-      <section className={styles.introduction}>
+      <section>
         <p>
           I work at <a href="https://www.reachcreative.com/" target="_blank">Reach Creative</a>, a Chicago-based design agency.
         </p>
