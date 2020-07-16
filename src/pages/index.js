ffd6e823a7eca37839fc8989890c744b1bbd85df
Delilah Clement <delilah@reachcreative.com>
2020-07-16 16:59:33 -0500

    refactor hero home background

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,6 @@ export default ({ isMobile }) => {
   return (
     <main id={styles.home}>
       <section className={classes(styles.hero, 'hero')}>
-        <img
-          className={styles['hero-bg-desktop']}
-          src="/images/home-hero-desktop.svg"
-        />
-        <img
-          className={styles['hero-bg-mobile']}
-          src="/images/home-hero-mobile.svg"
-        />
         <div className={styles['hero-copy']}>
           <h1>I'm Delilah</h1>
           <h2 className="h2">a front-end web developer</h2>
