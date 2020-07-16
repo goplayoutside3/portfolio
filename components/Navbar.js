bacd05509dbd886a5cac02ae3579951ec55e0948
Delilah Clement <delilah@reachcreative.com>
2020-07-16 13:06:12 -0500

    delete community page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Link from 'next/link'
 
 const Navbar = () => (
@@ -12,11 +11,6 @@ const Navbar = () => (
     <li>
       <Link href="/work">
         <a>Work</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/community">
-        <a>Community</a>
       </Link>
     </li>
     <li>
