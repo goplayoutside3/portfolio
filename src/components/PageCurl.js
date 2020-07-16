9bd516b8b62b4e946094549433d95c53bcc4e315
Delilah Clement <delilah@reachcreative.com>
2020-07-16 15:37:43 -0500

    re-style PageCurl

--- src/components/PageCurl.js
+++ src/components/PageCurl.js
@@ -1,6 +1,7 @@
 import '../gsap/gsap.min.js'
 import MorphSVGPlugin from '../gsap/MorphSVGPlugin.min.js'
 import { useEffect } from 'react'
+import styles from '../styles/components/pagecurl.module.scss'
 
 const PageCurl = ({ image = '', z = '1', title = '', site = '', isMobile }) => {
   const pageTopColor = 'white'
@@ -54,54 +55,51 @@ const PageCurl = ({ image = '', z = '1', title = '', site = '', isMobile }) => {
   return (
     <div
       id={`svg-wrapper${z}`}
-      className="svg-wrapper"
+      className={styles["svg-wrapper"]}
       style={{ zIndex: z }}
       onMouseOver={() => handleHover(z)}
       onMouseOut={() => handleLeave(z)}
     >
-      <a className="site-cta" href={site} target="_blank">
+      <a className={styles["site-cta"]} href={site} target="_blank">
         <div
-          className="thumbnail"
+          className={styles.thumbnail}
           style={{ backgroundImage: `url(${image})` }}
         />
-        {/* <div className="thumbnail cover"/> */}
         <svg
           id="closed-page-paths"
-          className="page"
+          className={styles.page}
           viewBox="0 0 512 598.5"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
             id={`page-closed-shadow${z}`}
-            className="page-closed-shadow"
+            className={styles["page-closed-shadow"]}
             d="M375.4,598.8l-204.5,0.5L39.1-0.3l336.3-0.5V598.8z"
             style={{ fill: 'black', opacity: 0.5 }}
           />
           <path
             id={`page-closed${z}`}
-            className="page-closed"
+            className={styles["page-closed"]}
             d="M399.6,598.8l-204.5,0.5L39.1-0.3l360.5-0.5V598.8z"
             style={{ fill: pageBottomColor }}
           />
         </svg>
 
-        <p className="title">{title}</p>
-
         <svg
           id="closed-curl-paths"
-          className="page"
+          className={styles.page}
           viewBox="0 0 512 598.5"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
             id={`curl-closed-shadow${z}`}
-            className="curl-closed-shadow"
+            className={styles["curl-closed-shadow"]}
             d="M366.2,470.5c-79,7-141.5,57-171.1,129l-156-600l16.2,0L366.2,470.5z"
             style={{ fill: 'black', opacity: 0.5 }}
           />
           <path
             id={`curl-closed${z}`}
-            className="curl-closed"
+            className={styles["curl-closed"]}
             d="M365.4,455.5c-81-1.8-140.6,72-170.2,144l-156-600l16.2,0L365.4,455.5z"
             style={{ fill: pageTopColor }}
           />
@@ -109,33 +107,33 @@ const PageCurl = ({ image = '', z = '1', title = '', site = '', isMobile }) => {
 
         <svg
           id="open-curl-paths"
-          className="page"
+          className={styles.page}
           viewBox="0 0 512 598.5"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
             id={`page-open-shadow${z}`}
-            className="page-open-shadow"
+            className={styles["page-open-shadow"]}
             d="M378.1,602.1L273,603L130,3l248.1-0.9V602.1z"
             style={{ fill: 'black', opacity: 0.5 }}
           />
           <path
             id={`page-open${z}`}
-            className="page-open"
+            className={styles["page-open"]}
             d="M400,599H294L125-0.1l275.1-0.4L400,599z"
             style={{ fill: pageBottomColor }}
           />
 
           <path
             id={`curl-open-shadow${z}`}
-            className="curl-open-shadow"
+            className={styles["curl-open-shadow"]}
             d="M513,525c-79,7-189.4,2.1-219,74.5L126-0.5h13.1L513,525z"
             style={{ fill: 'black', opacity: 0.5 }}
           />
 
           <path
             id={`curl-open${z}`}
-            className="curl-open"
+            className={styles["curl-open"]}
             d="M512,495.9c-81-1.8-188.4,31.9-218,103.6L125-0.5h14L512,495.9z"
             style={{ fill: pageTopColor }}
           />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,56 @@
 import styles from '../styles/layouts/home.module.scss'
 import classes from 'classnames'
+import { PageCurl } from '../components'
 
-export default () => {
+export default ({ isMobile }) => {
   return (
     <main id={styles.home}>
       <section className={classes(styles.hero, 'hero')}>
-        <img className={styles["hero-bg-desktop"]} src="/images/home-hero-desktop.svg" />
-        <img className={styles["hero-bg-mobile"]} src="/images/home-hero-mobile.svg" />
-        <div className={styles["hero-copy"]}>
+        <img
+          className={styles['hero-bg-desktop']}
+          src="/images/home-hero-desktop.svg"
+        />
+        <img
+          className={styles['hero-bg-mobile']}
+          src="/images/home-hero-mobile.svg"
+        />
+        <div className={styles['hero-copy']}>
           <h1>I'm Delilah</h1>
           <h2 className="h2">a front-end web developer</h2>
         </div>
       </section>
-      <section>
-        <p>
-          I work at <a href="https://www.reachcreative.com/" target="_blank">Reach Creative</a>, a Chicago-based design agency.
+      <section className={classes(styles.work, 'container')}>
+        <h2 className="h2">Professional Work</h2>
+        <p className="h4">
+          Select projects built as lead developer at{' '}
+          <a href="https://www.reachcreative.com/" target="_blank">
+            Reach Creative
+          </a>
+          , a Chicago-based design agency.
         </p>
+        <div className={styles['projects-cont']}>
+          <PageCurl
+            image="/images/northface2.jpg"
+            z="4"
+            title="Rewind Nature"
+            site="https://www.nationalgeographic.com/rewindnature/"
+            isMobile={isMobile}
+          />
+          <PageCurl
+            image="/images/mazda2.jpg"
+            z="3"
+            title="Inspiration in Every Mile"
+            site="https://www.nationalgeographic.com/mazda/"
+            isMobile={isMobile}
+          />
+          <PageCurl
+            image="/images/maine2.jpg"
+            z="2"
+            title="What's In Your Pack?"
+            site="https://www.nationalgeographic.com/whatsinyourpack/"
+            isMobile={isMobile}
+          />
+        </div>
       </section>
     </main>
   )
