f30c82abaf14acbf1b795236545715c194e38d45
Delilah Clement <delilahclement@gmail.com>
2021-05-12 17:05:39 -0500

    edit site text

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,6 @@ const Homepage = ({ isMobile }) => {
       </section>
       <section className={classes(styles.work, 'container')}>
         <h2 className="h2">Professional Work</h2>
-        <p className={classes("h4", styles['work-desc'])}>
-          Select projects built as primary developer at{' '}
-          <a href="https://www.reachcreative.com/" target="_blank">
-            Reach Creative
-          </a>
-          , a Chicago-based design agency.
-        </p>
         <div className={styles['projects-cont']}>
           <PageCurl
             image="/images/globe.jpg"
@@ -55,8 +48,8 @@ const Homepage = ({ isMobile }) => {
             <li>GSAP</li>
             <li>Next.js</li>
             <li>Headless CMS</li>
-            <li>Adobe Illustrator</li>
-            <li>Adobe Photoshop</li>
+            <li>Enzyme</li>
+            <li>Docker</li>
           </ul>
           <ul>
             <li>REST APIs</li>
